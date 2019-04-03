beb01b0566f6ebf7194998be3f560d7e380ea054
pollutedlake <psh950827@naver.com>
2019-04-04 01:53:16 +0900

    Event handling

---
+++ eventhandling/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EventPractice from './EventPractice';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <EventPractice/>
+      </div>
+    );
+  }
+}
+
+export default App;
